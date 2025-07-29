14a5d783152322b660a8442d118d48a046a5855d
Jeremy Hustlin <82581901+JeremyHustlin@users.noreply.github.com>
2025-07-29 23:21:24 +0300

    toaster

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { Toaster } from "@/components/ui/toaster"
 import './styles/globals.css'
-import { BrowserRouter } from 'react-router-dom'
 import './lib/scheduler' // Import scheduler to start it
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <Toaster />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
